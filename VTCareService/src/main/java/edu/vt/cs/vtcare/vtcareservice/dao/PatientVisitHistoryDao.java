1191fbacea4091785d5724523a3839555142c7df
Akshay Phogat <akshayphogat@Akshays-MacBook-Air.local>
2022-11-22 22:54:19 -0500

    VC-91 Created POST API to Persist Patient Visit History.

--- VTCareService/src/main/java/edu/vt/cs/vtcare/vtcareservice/dao/PatientVisitHistoryDao.java
+++ VTCareService/src/main/java/edu/vt/cs/vtcare/vtcareservice/dao/PatientVisitHistoryDao.java
@@ -3,10 +3,7 @@ package edu.vt.cs.vtcare.vtcareservice.dao;
 import edu.vt.cs.vtcare.vtcareservice.db.VTCareJDBC;
 import edu.vt.cs.vtcare.vtcareservice.models.PatientVisitHistory;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +20,10 @@ public class PatientVisitHistoryDao {
 
     private static final String FIND_HISTORY_BY_PATIENT_ID_SQL =
             "SELECT * FROM patient_visit_histories where patient_id = ?";
+
+    private static final String INSERT_HISTORY_SQL =
+            "INSERT INTO patient_visit_histories(patient_id, appointment_date, provider_name, diagnosis, prescription, notes)" +
+                    "VALUES(?, ?, ?, ?, ?, ?)";
 
     /**
      * Finds a visit history by patientId
@@ -60,5 +61,31 @@ public class PatientVisitHistoryDao {
         String notes = resultSet.getString("notes");
 
         return new PatientVisitHistory(id, patientId, apptDate, providerName, diagnosis, prescription, notes);
+    }
+
+    /**
+     * Persists patient's history into the database.
+     * @param patientVisitHistory
+     * @return patientVisitHistory generated ID.
+     */
+    public long persistPatientHistory(PatientVisitHistory patientVisitHistory) {
+        long historyId = -1;
+        try (PreparedStatement statement = connection.prepareStatement(INSERT_HISTORY_SQL, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setLong(1, patientVisitHistory.getPatientId());
+            statement.setDate(2, java.sql.Date.valueOf(patientVisitHistory.getApptDate()));
+            statement.setString(3, patientVisitHistory.getProviderName());
+            statement.setString(4, patientVisitHistory.getDiagnosis());
+            statement.setString(5, patientVisitHistory.getPrescription());
+            statement.setString(6, patientVisitHistory.getNotes());
+            statement.executeUpdate();
+
+            ResultSet rs = statement.getGeneratedKeys();
+            if (rs.next()) {
+                historyId = rs.getLong(1);
+            }
+        } catch (SQLException e) {
+            System.out.println("Encountered problem creating a new patient " + e);
+        }
+        return historyId;
     }
 }
